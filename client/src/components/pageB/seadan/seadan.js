ed26d78dbc9e3818eb9d26617fa2249c76c1f7a0
Bharanidharan C <59705880+Bharanidharan7@users.noreply.github.com>
2021-09-01 21:47:11 +0530

    Delete client/src/components/pageB/seadan directory

--- client/src/components/pageB/seadan/seadan.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SeadanModal = (props) => (
-    <div>
-            <button  onClick={props.handlePick} disabled={!props.hasOptions}>4 seats</button>
-        </div>
-    );
-
-export default SeadanModal
